30222e057dcf3d873f0fd9c21cd4e78dd1f3224d
David Charif <davidcharif@hotmail.com>
2022-06-30 23:42:23 -0500

    Main structure and deployiment

--- src/Components/ProductSelect.jsx
+++ src/Components/ProductSelect.jsx
@@ -3,23 +3,50 @@ import { ProductSelectStyled } from '../Styles/ProductSelectStyled'
 import ItemsSelected from './ItemsSelected'
 
 const ProductSelect = () => {
+  const [selectedItems, setSelectedItems] = React.useState('1')
+  const [title, setTitle] = React.useState(getTitle(selectedItems))
+
+
+ React.useEffect(() => {
+    setTitle(getTitle(selectedItems))   
+  }
+  , [selectedItems])
+  
+  const handleClick = ({target:{value}}) => {
+    setSelectedItems(value)    
+  }
+  
   return (
     <ProductSelectStyled>
       <div className="nav">
-        <button type='button'>
+        <button type='button' value={1} onClick={handleClick}>
           Silla de ruedas
         </button>
-        <button type='button'>
+        <button type='button' value={2} onClick={handleClick}>
           Terapia en el hogar
         </button>
-        <button type='button'>
+        <button type='button' value={3} onClick={handleClick}>
           Movilidad
         </button>
       </div>
       <div className="backgroundImage"></div>
-      <ItemsSelected title={'SILLA DE RUEDAS'} />
+      <ItemsSelected title={title} />
     </ProductSelectStyled>
   )
 }
 
 export default ProductSelect
+
+function getTitle(num) {
+  switch(num){
+    case '1':
+      return 'Silla de ruedas'
+    case '2':
+      return 'Terapia en el hogar'
+    case '3':
+      return 'Movilidad'
+    default:
+      console.log('error')
+}
+}
+
